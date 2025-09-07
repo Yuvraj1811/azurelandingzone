e111ca4682d6ced7eff64e8f74452bbe234df93c
Yuvraj1811 <ys2029472@gmail.com>
2025-09-08 00:39:54 +0530

    done changes in variable and provider

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 terraform {
-     backend "azurerm" {
+  backend "azurerm" {
     resource_group_name  = "rg-tfbackend"
     storage_account_name = "tfbackendstrg"
     container_name       = "tfcontainer"
@@ -7,24 +7,19 @@ terraform {
   }
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.43.0"
     }
   }
 }
 
-variables "client_id"{}
-variables "client_secret"{}
-variables "subscription_id"{}
-variables "tenant_id"{}
-
 
 provider "azurerm" {
-  features{}
+  features {}
   subscription_id = var.subscription_id
-  client_id = var.client_id
-  client_secret = var.client_secret
-  tenant_id = var.tenant_id
-  
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+
 }
 
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+subscription_id = "1ef3481e-bfe1-4160-aeeb-862132dc8f0e"
+client_id       = "da8b7712-3f70-4aac-b5c4-5a5ec89a4459"
+client_secret   = "x5g8Q~jdMX.xJMWeR0wvVOOPQjd9hGGtbaItCbVG"
+tenant_id       = "43b2ab88-f668-409f-ba76-8db3e53453ae"
